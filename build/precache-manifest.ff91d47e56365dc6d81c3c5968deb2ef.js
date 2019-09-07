6230975b0aad9e195e5a547f93e317e136a3e9ed
zhangbx <496691544@qq.com>
2019-09-07 16:08:33 +0800

    update build

---
+++ build/precache-manifest.ff91d47e56365dc6d81c3c5968deb2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e7a87a3c29ea27d21f5b1cafd4dc85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e8df63e66ae0d1be8a",
+    "url": "/static/css/main.1c32faed.chunk.css"
+  },
+  {
+    "revision": "89a30d4be1c28419dddd",
+    "url": "/static/js/2.731091af.chunk.js"
+  },
+  {
+    "revision": "91e8df63e66ae0d1be8a",
+    "url": "/static/js/main.9f4b9a96.chunk.js"
+  },
+  {
+    "revision": "b2e636eb66c049f54b40",
+    "url": "/static/js/runtime~main.93ae9baf.js"
+  }
+]);
